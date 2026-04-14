399fe692fbb620b5addbac19e82b0d988f630fd4
JohnBadri <135007003+JohnBadri@users.noreply.github.com>
2026-04-14 20:42:41 +1000

    added model to handle data operations

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'summary.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +13,40 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text('Wikipedia Flutter'),
+        ),
+        body: const Center(
+          child: Text('Loading...'),
         ),
       ),
     );
   }
 }
+
+// Model: Handles data operations.
+class ArticleModel {
+  Future<Summary> getRandomArticleSummary() async {
+    final uri = Uri.https(
+      'en.wikipedia.org',
+      '/api/rest_v1/page/random/summary',
+    );
+    final response = await http.get(uri);
+
+    if (response.statusCode != 200) {
+      throw HttpException('Failed to update resources');
+    }
+
+    return Summary.fromJson(
+      jsonDecode(response.body),
+    );
+  }
+}
+
+// View: Displays the UI.
+class ArticleViewModel {}
+
+// ViewModel: Manages state and connects the two.
+class ArticleView {}
